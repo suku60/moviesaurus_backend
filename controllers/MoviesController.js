d7190db426a59e40838d33bcd60dfc388d4c1cff
suku60 <juanmaxp2@gmail.com>
2022-02-27 19:37:33 +0100

    MoviesController updated.

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -3,3 +3,35 @@ const { Op } = require('sequelize');
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth');
 const bcrypt = require('bcrypt');
+
+const MoviesController = {};
+
+// (Read) Show all movies
+
+MoviesController.showAllMovies = (req, res) => {};
+
+// - Show newest movies
+
+MoviesController.newestMovies = (req, res) => {};
+
+// - Show filtered by adult rating
+
+MoviesController.adultMovies = (req, res) => {};
+
+// (Create) Store a movie
+
+MoviesController.createMovie = (req, res) => {};
+
+// (Update) Modify movie data
+
+MoviesController.updateMovie = (req, res) => {};
+
+// (Delete) movies stored
+
+// - All
+
+MoviesController.deleteAllMovies = (req, res) => {};
+
+// - Filtered by Id
+
+MoviesController.deleteMovie = (req, res) => {};
